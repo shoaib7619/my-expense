0516458dcb958b1be06203c29ad285e690cf9192
Muhammad Shoaib <94863313+shoaib7619@users.noreply.github.com>
2023-12-18 19:11:15 +0500

    code added

--- README.md
+++
@@ -1,2 +0,0 @@
-# my-expense
-Daily expense mern app
--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-// import userRoutes from "./routes/userRoutes.js";
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors');
 const  dotenv= require('dotenv')
 const connectDb = require('./config/connection')
 const path = require('path')
-dotenv.config()
+
+dotenv.config();
+
 
 //database connection
 connectDb()
@@ -18,29 +19,25 @@ app.use(morgan('dev'))
 app.use(express.json())
 app.use(cors())
 
-//static file
-app.use(express.static(path.join(__dirname, "./client/build")));
-
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
-
-app.get("/*", function(_, res) {
-  res.sendFile(path.join(__dirname,"./client/build/index.html"),function(err){
-     res.status(500).send(err)
-  }
-    );
-});
 
 //routes for user
-app.use("/api/v1/users", require("./routes/userRoute"));
+app.use('/api/v1/users',require('.routes/userRoute'))
 
-//transections routes
-app.use("/api/v1/transections", require("./routes/transectionRoutes"));
+// Transactions routes
+app.use('/api/v1/transactions', require('./routes/transactionsRoutes'));
+
+//static file
+app.use(express(path.join(__dirname,'./client/build')))
+
+// Catch-all route for serving React app
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './client/build/index.html'));
+});
+
 
 //port
 
-const PORT =  process.env.PORT || 8080
+const PORT = 8080 || process.env.PORT
 
 //listen function
 app.listen(PORT, () => {
